8fe9c8b1eb053428ad88f2da916ef4101cd0d643
roaa94 <roaa94khaddam@gmail.com>
2021-03-09 17:33:20 +0300

    working on splash screen

--- lib/UI/screens/SplashScreen.dart
+++ lib/UI/screens/SplashScreen.dart
@@ -3,11 +3,26 @@ import 'package:flutter/material.dart';
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
-      body: Center(
-        child: Image.asset(
-          'assets/images/tiptop-logo.png',
-          width: 140,
+      body: Container(
+        height: screenSize.height,
+        width: screenSize.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/splash-bg.png"),
+            alignment: Alignment.bottomCenter,
+          )
+        ),
+        child: Column(
+          children: [
+            SizedBox(height: screenSize.height * 0.2),
+            Image.asset(
+              'assets/images/tiptop-logo.png',
+              width: screenSize.width / 2.5,
+            ),
+          ],
         ),
       )
     );
