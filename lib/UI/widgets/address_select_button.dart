4a19faa863980c904a7280356c888d14b7e6a426
roaa94 <roaa94khaddam@gmail.com>
2021-03-14 13:29:56 +0300

    add border to address select button

--- lib/UI/widgets/address_select_button.dart
+++ lib/UI/widgets/address_select_button.dart
@@ -11,117 +11,124 @@ class AddressSelectButton extends StatelessWidget {
   Widget build(BuildContext context) {
     AppProvider appProvider = Provider.of<AppProvider>(context);
 
-    return Row(
-      children: [
-        Expanded(
-          flex: 7,
-          child: InkWell(
-            onTap: () {
-              //Todo: Open Addresses modal/bottom sheet
-            },
-            child: Container(
-              padding: EdgeInsets.only(
-                left: appProvider.dir == 'ltr' ? 17 : 0,
-                right: appProvider.dir == 'ltr' ? 0 : 17,
-                top: 10,
-                bottom: 10,
-              ),
-              child: Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.only(
-                      right: appProvider.dir == 'ltr' ? 10 : 0,
-                      left: appProvider.dir == 'ltr' ? 0 : 10,
-                    ),
-                    margin: EdgeInsets.only(
-                      right: appProvider.dir == 'ltr' ? 10 : 0,
-                      left: appProvider.dir == 'ltr' ? 0 : 10,
-                    ),
-                    child: Image(
-                      image: AssetImage('assets/images/address-home-icon.png'),
-                      width: 37,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border(
-                        right: BorderSide(
-                          width: 1,
-                          color: AppColors.border,
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(width: 2, color: AppColors.primary),
+        ),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 7,
+            child: InkWell(
+              onTap: () {
+                //Todo: Open Addresses modal/bottom sheet
+              },
+              child: Container(
+                padding: EdgeInsets.only(
+                  left: appProvider.dir == 'ltr' ? 17 : 0,
+                  right: appProvider.dir == 'ltr' ? 0 : 17,
+                  top: 10,
+                  bottom: 10,
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(
+                        right: appProvider.dir == 'ltr' ? 10 : 0,
+                        left: appProvider.dir == 'ltr' ? 0 : 10,
+                      ),
+                      margin: EdgeInsets.only(
+                        right: appProvider.dir == 'ltr' ? 10 : 0,
+                        left: appProvider.dir == 'ltr' ? 0 : 10,
+                      ),
+                      child: Image(
+                        image: AssetImage('assets/images/address-home-icon.png'),
+                        width: 37,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border(
+                          right: BorderSide(
+                            width: 1,
+                            color: AppColors.border,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Address',
-                            style: AppTextStyles.subtitleBold,
-                          ),
-                          Row(
-                            children: [
-                              Text(
-                                'Home',
-                                style: AppTextStyles.subtitle,
-                              ),
-                              SizedBox(width: 5),
-                              Expanded(
-                                child: Text(
-                                  'Sultan Selim Mah. Tuna Cad. Yasam Evleri Residence',
-                                  style: AppTextStyles.subtitle50,
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
+                    Expanded(
+                      child: Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Address',
+                              style: AppTextStyles.subtitleBold,
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  'Home',
+                                  style: AppTextStyles.subtitle,
                                 ),
-                              )
-                            ],
-                          ),
-                        ],
+                                SizedBox(width: 5),
+                                Expanded(
+                                  child: Text(
+                                    'Sultan Selim Mah. Tuna Cad. Yasam Evleri Residence',
+                                    style: AppTextStyles.subtitle50,
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 5),
+                      child: AppIcon.icon(FontAwesomeIcons.angleRight),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              padding: EdgeInsets.only(
+                right: appProvider.dir == 'ltr' ? 17 : 0,
+                left: appProvider.dir == 'ltr' ? 0 : 17,
+                top: 10,
+                bottom: 10,
+              ),
+              color: AppColors.primary,
+              child: Column(
+                children: [
+                  Text(
+                    'ETA',
+                    style: AppTextStyles.subtitleWhite,
                   ),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 5),
-                    child: AppIcon.icon(FontAwesomeIcons.angleRight),
+                  SizedBox(height: 5),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('11', style: AppTextStyles.h2White),
+                      SizedBox(width: 3),
+                      Text(
+                        'min',
+                        style: AppTextStyles.subtitleWhiteBold,
+                      ),
+                    ],
                   ),
                 ],
               ),
             ),
-          ),
-        ),
-        Expanded(
-          flex: 2,
-          child: Container(
-            padding: EdgeInsets.only(
-              right: appProvider.dir == 'ltr' ? 17 : 0,
-              left: appProvider.dir == 'ltr' ? 0 : 17,
-              top: 10,
-              bottom: 10,
-            ),
-            color: AppColors.primary,
-            child: Column(
-              children: [
-                Text(
-                  'ETA',
-                  style: AppTextStyles.subtitleWhite,
-                ),
-                SizedBox(height: 5),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('11', style: AppTextStyles.h2White),
-                    SizedBox(width: 3),
-                    Text(
-                      'min',
-                      style: AppTextStyles.subtitleWhiteBold,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
