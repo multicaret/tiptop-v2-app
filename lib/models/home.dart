15999d16ba693cde80f1aa2ac15d734b86d56353
roaa94 <roaa94khaddam@gmail.com>
2021-03-15 13:22:49 +0300

    modify home endpoint models

--- lib/models/home.dart
+++ lib/models/home.dart
@@ -1,5 +1,11 @@
 import 'dart:convert';
 
+import 'package:tiptop_v2/models/models.dart';
+
+import 'package:tiptop_v2/models/models.dart';
+
+import 'package:tiptop_v2/models/models.dart';
+
 import 'category.dart';
 
 HomeDataResponse homeDataResponseFromJson(String str) => HomeDataResponse.fromJson(json.decode(str));
@@ -48,7 +54,7 @@ class HomeData {
   List<dynamic> addresses;
   List<dynamic> slides;
   EstimatedArrivalTime estimatedArrivalTime;
-  dynamic branch;
+  Branch branch;
   dynamic distance;
   bool hasAvailableBranchesNow;
   List<Category> categories;
@@ -57,7 +63,7 @@ class HomeData {
     addresses: List<dynamic>.from(json["addresses"].map((x) => x)),
     slides: List<dynamic>.from(json["slides"].map((x) => x)),
     estimatedArrivalTime: EstimatedArrivalTime.fromJson(json["estimated_arrival_time"]),
-    branch: json["branch"],
+    branch: Branch.fromJson(json["branch"]),
     distance: json["distance"],
     hasAvailableBranchesNow: json["hasAvailableBranchesNow"],
     categories: List<Category>.from(json["categories"].map((x) => Category.fromJson(x))),
@@ -67,7 +73,7 @@ class HomeData {
     "addresses": List<dynamic>.from(addresses.map((x) => x)),
     "slides": List<dynamic>.from(slides.map((x) => x)),
     "estimated_arrival_time": estimatedArrivalTime.toJson(),
-    "branch": branch,
+    "branch": branch.toJson(),
     "distance": distance,
     "hasAvailableBranchesNow": hasAvailableBranchesNow,
     "categories": List<dynamic>.from(categories.map((x) => x.toJson())),
@@ -91,5 +97,146 @@ class EstimatedArrivalTime {
   Map<String, dynamic> toJson() => {
     "value": value,
     "unit": unit,
+  };
+}
+
+class Branch {
+  Branch({
+    this.id,
+    this.regionId,
+    this.cityId,
+    this.minimumOrder,
+    this.underMinimumOrderDeliveryFee,
+    this.fixedDeliveryFee,
+    this.primaryPhoneNumber,
+    this.secondaryPhoneNumber,
+    this.whatsappPhoneNumber,
+    this.latitude,
+    this.longitude,
+    this.chain,
+  });
+
+  int id;
+  int regionId;
+  int cityId;
+  IntRawStringFormatted minimumOrder;
+  IntRawStringFormatted underMinimumOrderDeliveryFee;
+  IntRawStringFormatted fixedDeliveryFee;
+  String primaryPhoneNumber;
+  String secondaryPhoneNumber;
+  String whatsappPhoneNumber;
+  String latitude;
+  String longitude;
+  Chain chain;
+
+  factory Branch.fromJson(Map<String, dynamic> json) => Branch(
+    id: json["id"],
+    regionId: json["regionId"],
+    cityId: json["cityId"],
+    minimumOrder: IntRawStringFormatted.fromJson(json["minimumOrder"]),
+    underMinimumOrderDeliveryFee: IntRawStringFormatted.fromJson(json["underMinimumOrderDeliveryFee"]),
+    fixedDeliveryFee: IntRawStringFormatted.fromJson(json["fixedDeliveryFee"]),
+    primaryPhoneNumber: json["primaryPhoneNumber"],
+    secondaryPhoneNumber: json["secondaryPhoneNumber"],
+    whatsappPhoneNumber: json["whatsappPhoneNumber"],
+    latitude: json["latitude"],
+    longitude: json["longitude"],
+    chain: Chain.fromJson(json["chain"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "regionId": regionId,
+    "cityId": cityId,
+    "minimumOrder": minimumOrder.toJson(),
+    "underMinimumOrderDeliveryFee": underMinimumOrderDeliveryFee.toJson(),
+    "fixedDeliveryFee": fixedDeliveryFee.toJson(),
+    "primaryPhoneNumber": primaryPhoneNumber,
+    "secondaryPhoneNumber": secondaryPhoneNumber,
+    "whatsappPhoneNumber": whatsappPhoneNumber,
+    "latitude": latitude,
+    "longitude": longitude,
+    "chain": chain.toJson(),
+  };
+}
+
+class Chain {
+  Chain({
+    this.id,
+    this.regionId,
+    this.cityId,
+    this.currencyId,
+    this.primaryPhoneNumber,
+    this.secondaryPhoneNumber,
+    this.whatsappPhoneNumber,
+    this.primaryColor,
+    this.secondaryColor,
+    this.numberOfItemsOnMobileGridView,
+    this.media,
+  });
+
+  int id;
+  int regionId;
+  int cityId;
+  int currencyId;
+  String primaryPhoneNumber;
+  String secondaryPhoneNumber;
+  String whatsappPhoneNumber;
+  String primaryColor;
+  String secondaryColor;
+  int numberOfItemsOnMobileGridView;
+  Media media;
+
+  factory Chain.fromJson(Map<String, dynamic> json) => Chain(
+    id: json["id"],
+    regionId: json["regionId"],
+    cityId: json["cityId"],
+    currencyId: json["currencyId"],
+    primaryPhoneNumber: json["primaryPhoneNumber"],
+    secondaryPhoneNumber: json["secondaryPhoneNumber"],
+    whatsappPhoneNumber: json["whatsappPhoneNumber"],
+    primaryColor: json["primaryColor"],
+    secondaryColor: json["secondaryColor"],
+    numberOfItemsOnMobileGridView: json["numberOfItemsOnMobileGridView"],
+    media: Media.fromJson(json["media"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "regionId": regionId,
+    "cityId": cityId,
+    "currencyId": currencyId,
+    "primaryPhoneNumber": primaryPhoneNumber,
+    "secondaryPhoneNumber": secondaryPhoneNumber,
+    "whatsappPhoneNumber": whatsappPhoneNumber,
+    "primaryColor": primaryColor,
+    "secondaryColor": secondaryColor,
+    "numberOfItemsOnMobileGridView": numberOfItemsOnMobileGridView,
+    "media": media.toJson(),
+  };
+}
+
+class Media {
+  Media({
+    this.logo,
+    // this.cover,
+    this.gallery,
+  });
+
+  String logo;
+  //Todo: uncomment when filled
+  // String cover;
+  List<dynamic> gallery;
+
+  factory Media.fromJson(Map<String, dynamic> json) => Media(
+    logo: json["logo"],
+    // cover: json["cover"],
+    gallery: List<dynamic>.from(json["gallery"].map((x) => x)),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "logo": logo,
+    // "cover": cover,
+    "gallery": List<dynamic>.from(gallery.map((x) => x)),
   };
 }
