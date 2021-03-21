bd9519f7f39150da7ef3b0f0ea785590f1322477
Suhail MSK2 <msk286@gmail.com>
2021-03-21 21:10:53 +0300

    work on search api

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -3,15 +3,18 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:tiptop_v2/models/category.dart';
 import 'package:tiptop_v2/models/product.dart';
+import 'package:tiptop_v2/models/searched_product.dart';
 import 'package:tiptop_v2/providers/app_provider.dart';
 import 'package:tiptop_v2/utils/http_exception.dart';
 
 class ProductsProvider with ChangeNotifier {
   ProductsDataResponse productsDataResponse;
+  ProductsResponse searchedProductsDataResponse;
   ParentsData parentsData;
   Category selectedParent;
   List<Category> parents = [];
   List<Category> selectedParentChildCategories = [];
+  List<ProductData> searchedProducts = [];
 
   Future<void> fetchAndSetParentsAndProducts(int selectedParentCategoryId) async {
     final endpoint = 'categories/$selectedParentCategoryId/products';
@@ -27,5 +30,30 @@ class ProductsProvider with ChangeNotifier {
     parents = parentsData.parents;
     selectedParent = parentsData.selectedParent;
     selectedParentChildCategories = selectedParent.childCategories;
+  }
+
+  Future<void> fetchSearchedProducts(searchQuery) async {
+    final endpoint = 'products?q=$searchQuery';
+
+    try {
+      final responseData = await AppProvider().get(endpoint: endpoint);
+      searchedProductsDataResponse = productsResponseFromMap(json.encode(responseData));
+      searchedProducts = searchedProductsDataResponse.data;
+
+      if (searchedProductsDataResponse.data == null || searchedProductsDataResponse.status != 200) {
+        throw HttpException(title: 'Error', message: searchedProductsDataResponse.message);
+      }
+    } catch (e) {
+      print('@e Error');
+      print(e);
+    }
+
+    // searchedProductsResponse = productDataResponseFromJson(json.encode(responseData));
+
+    // if (searchedProductsResponse == null || productsDataResponse.status != 200) {
+    //   throw HttpException(title: 'Error', message: productsDataResponse.message);
+    // }
+    //
+    // searchedProducts =
   }
 }
