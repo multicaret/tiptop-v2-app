a8d3bb974a614531ca5c1c605817602cca83a5c3
KawsaraMohamed MK <mkwsra@gmail.com>
2021-03-21 15:28:55 +0300

    add webview for support tab

--- lib/UI/pages/support_page.dart
+++ lib/UI/pages/support_page.dart
@@ -1,18 +1,26 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:tiptop_v2/UI/widgets/app_scaffold.dart';
 import 'package:tiptop_v2/i18n/translations.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class SupportPage extends StatelessWidget {
   static const routeName = '/support';
+  Completer<WebViewController> _controller = Completer<WebViewController>();
 
   @override
   Widget build(BuildContext context) {
     return AppScaffold(
+      hasCurve: false,
       appBar: AppBar(
         title: Text(Translations.of(context).get('Support')),
       ),
-      body: Center(
-        child: Text('Support Page'),
+      body: WebView(
+        initialUrl: 'https://titan.trytiptop.app/en/support',
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller.complete(webViewController);
+        },
       ),
     );
   }
