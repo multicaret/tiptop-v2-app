559ac7456121137b9e5c8c82f5044679b0a40359
roaa94 <roaa94khaddam@gmail.com>
2021-03-22 18:15:51 +0300

    extract order button widget

--- lib/UI/pages/cart_page.dart
+++ lib/UI/pages/cart_page.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:tiptop_v2/UI/widgets/app_scaffold.dart';
 import 'package:tiptop_v2/UI/widgets/cart_product_item.dart';
+import 'package:tiptop_v2/UI/widgets/order_button.dart';
 import 'package:tiptop_v2/i18n/translations.dart';
 import 'package:tiptop_v2/providers/app_provider.dart';
 import 'package:tiptop_v2/providers/cart_provider.dart';
-import 'package:tiptop_v2/utils/styles/app_colors.dart';
 import 'package:tiptop_v2/utils/styles/app_icon.dart';
-import 'package:tiptop_v2/utils/styles/app_text_styles.dart';
 
 class CartPage extends StatefulWidget {
   static const routeName = '/cart';
@@ -85,64 +83,10 @@ class _CartPageState extends State<CartPage> {
               ),
             ),
           ),
-          Positioned(
-              bottom: 0,
-              right: 0,
-              left: 0,
-              child: Container(
-                color: AppColors.primary,
-                padding: EdgeInsets.only(top: 20, bottom: 40, left: 17, right: 17),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      child: Container(
-                        height: 45,
-                        child: Text(Translations.of(context).get('Order Now')),
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          color: AppColors.secondaryDark,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(appProvider.isRTL ? 0 : 8),
-                            bottomLeft: Radius.circular(appProvider.isRTL ? 0 : 8),
-                            topRight: Radius.circular(appProvider.isRTL ? 8 : 0),
-                            bottomRight: Radius.circular(appProvider.isRTL ? 8 : 0),
-                          ),
-                          boxShadow: [
-                            BoxShadow(color: AppColors.shadowDark, blurRadius: 6),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      height: 45,
-                      alignment: Alignment.center,
-                      padding: EdgeInsets.symmetric(horizontal: 17),
-                      decoration: BoxDecoration(
-                        color: AppColors.white,
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(appProvider.isRTL ? 0 : 8),
-                          bottomRight: Radius.circular(appProvider.isRTL ? 0 : 8),
-                          topLeft: Radius.circular(appProvider.isRTL ? 8 : 0),
-                          bottomLeft: Radius.circular(appProvider.isRTL ? 8 : 0),
-                        ),
-                        boxShadow: [
-                          BoxShadow(color: AppColors.shadowDark, blurRadius: 6),
-                        ],
-                      ),
-                      child: cartProvider.isLoadingAddRemoveRequest
-                          ? SpinKitThreeBounce(
-                              color: AppColors.primary,
-                              size: 20,
-                            )
-                          : Text(
-                              cartProvider.cartTotal,
-                              style: AppTextStyles.h2.copyWith(fontWeight: FontWeight.w400),
-                            ),
-                    )
-                  ],
-                ),
-              ))
+          OrderButton(
+            cartProvider: cartProvider,
+            isRTL: appProvider.isRTL,
+          ),
         ],
       ),
     );
---
+++ lib/UI/widgets/order_button.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:tiptop_v2/i18n/translations.dart';
+import 'package:tiptop_v2/providers/cart_provider.dart';
+import 'package:tiptop_v2/utils/styles/app_colors.dart';
+import 'package:tiptop_v2/utils/styles/app_text_styles.dart';
+
+class OrderButton extends StatelessWidget {
+  final CartProvider cartProvider;
+  final bool isRTL;
+
+  OrderButton({
+    @required this.cartProvider,
+    @required this.isRTL,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+        bottom: 0,
+        right: 0,
+        left: 0,
+        child: Container(
+          color: AppColors.primary,
+          padding: EdgeInsets.only(top: 20, bottom: 40, left: 17, right: 17),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Container(
+                  height: 45,
+                  child: Text(Translations.of(context).get('Order Now')),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: AppColors.secondaryDark,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(isRTL ? 0 : 8),
+                      bottomLeft: Radius.circular(isRTL ? 0 : 8),
+                      topRight: Radius.circular(isRTL ? 8 : 0),
+                      bottomRight: Radius.circular(isRTL ? 8 : 0),
+                    ),
+                    boxShadow: [
+                      BoxShadow(color: AppColors.shadowDark, blurRadius: 6),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                height: 45,
+                alignment: Alignment.center,
+                padding: EdgeInsets.symmetric(horizontal: 17),
+                decoration: BoxDecoration(
+                  color: AppColors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(isRTL ? 0 : 8),
+                    bottomRight: Radius.circular(isRTL ? 0 : 8),
+                    topLeft: Radius.circular(isRTL ? 8 : 0),
+                    bottomLeft: Radius.circular(isRTL ? 8 : 0),
+                  ),
+                  boxShadow: [
+                    BoxShadow(color: AppColors.shadowDark, blurRadius: 6),
+                  ],
+                ),
+                child: cartProvider.isLoadingAddRemoveRequest
+                    ? SpinKitThreeBounce(
+                        color: AppColors.primary,
+                        size: 20,
+                      )
+                    : Text(
+                        cartProvider.cartTotal,
+                        style: AppTextStyles.h2.copyWith(fontWeight: FontWeight.w400),
+                      ),
+              )
+            ],
+          ),
+        ));
+  }
+}
