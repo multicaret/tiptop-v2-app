b760a5df520d7fcabfd88f8b0172a52895bc3ffb
roaa94 <roaa94khaddam@gmail.com>
2021-03-24 04:47:27 +0300

    set up addresses index API request function and models

--- lib/providers/addresses_provider.dart
+++ lib/providers/addresses_provider.dart
@@ -5,13 +5,17 @@ import 'package:tiptop_v2/providers/app_provider.dart';
 import 'package:tiptop_v2/utils/http_exception.dart';
 
 class AddressesProvider with ChangeNotifier {
+  CreateAddressResponseData createAddressResponseData;
+  CreateAddressData createAddressData;
+
   List<City> cities = [];
   List<Region> regions = [];
   City selectedCity;
   Region selectedRegion;
 
-  CreateAddressResponseData createAddressResponseData;
-  CreateAddressData createAddressData;
+  AddressesResponseData addressesResponseData;
+  List<Address> addresses = [];
+  List<Kind> kinds = [];
 
   Future<dynamic> createAddress(AppProvider appProvider, {double lat, double long}) async {
     final endpoint = 'profile/addresses/create';
@@ -39,6 +43,24 @@ class AddressesProvider with ChangeNotifier {
     selectedCity = createAddressData.selectedCity;
     selectedRegion = createAddressData.selectedRegion;
 
+    notifyListeners();
+  }
+
+  Future<dynamic> fetchAndSetAddresses(AppProvider appProvider) async {
+    final endpoint = 'profile/addresses';
+    final responseData = await appProvider.get(endpoint: endpoint, withToken: true);
+    addressesResponseData = AddressesResponseData.fromJson(responseData);
+
+    if (responseData == 401) {
+      return 401;
+    }
+
+    if (addressesResponseData.addressesData == null || addressesResponseData.status != 200) {
+      throw HttpException(title: 'Error', message: addressesResponseData.message);
+    }
+
+    addresses = addressesResponseData.addressesData.addresses;
+    kinds = addressesResponseData.addressesData.kinds;
     notifyListeners();
   }
 }
