843dca7208eec85e0520ae0ce9200740e5024596
KawsaraMohamed MK <mkwsra@gmail.com>
2021-03-25 10:12:51 +0300

    implement search

--- lib/UI/pages/search_page.dart
+++ lib/UI/pages/search_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:tiptop_v2/UI/widgets/app_loader.dart';
 import 'package:tiptop_v2/UI/widgets/app_scaffold.dart';
 import 'package:tiptop_v2/UI/widgets/input/app_text_field.dart';
+import 'package:tiptop_v2/UI/widgets/product_item.dart';
+import 'package:tiptop_v2/UI/widgets/products-screen/child_category_products.dart';
 import 'package:tiptop_v2/i18n/translations.dart';
-import 'package:tiptop_v2/models/searched_product.dart';
+import 'package:tiptop_v2/models/product.dart';
 import 'package:tiptop_v2/providers/products_provider.dart';
 import 'package:tiptop_v2/utils/helper.dart';
 
@@ -19,20 +22,29 @@ class _SearchPageState extends State<SearchPage> {
   String searchQuery = '';
 
   ProductsProvider productsProvider;
-  List<ProductData> products = [];
+  List<Product> _products = [];
   bool _isLoading = false;
 
+  @override
+  void dispose() {
+    super.dispose();
+    setState(() {
+      _products = [];
+    });
+    print("Dispose Search");
+  }
+
+
   Future<void> _fetchProductsData(String _searchQuery) async {
-    // Todo: for UI
-    // setState(() {
-    //   _isLoading = true;
-    // });
+    setState(() {
+      _isLoading = true;
+    });
     await productsProvider.fetchSearchedProducts(_searchQuery);
-    products = productsProvider.searchedProducts;
+    _products = productsProvider.searchedProducts;
     // Todo: for UI
-    // setState(() {
-    //   _isLoading = false;
-    // });
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   @override
@@ -48,16 +60,23 @@ class _SearchPageState extends State<SearchPage> {
       appBar: AppBar(
         title: Text(Translations.of(context).get('Search')),
       ),
-      body: Center(
-        child: AppTextField(
-          labelText: 'Quick Search',
-          hasInnerLabel: true,
-          prefixIconData: FontAwesomeIcons.search,
-          hasClearIcon: true,
-          required: true,
-          onFieldSubmitted: (String searchQuery) => _mainSearch(searchQuery),
-        ),
-      ),
+      body: _isLoading
+          ? AppLoader()
+          : Center(
+              child: Column(
+                children: [
+                  AppTextField(
+                    labelText: 'Quick Search',
+                    hasInnerLabel: true,
+                    prefixIconData: FontAwesomeIcons.search,
+                    hasClearIcon: true,
+                    required: true,
+                    onFieldSubmitted: (String searchQuery) => _mainSearch(searchQuery),
+                  ),
+                  if (_products.length > 1) ProductsGridView(products: _products),
+                ],
+              ),
+            ),
     );
   }
 
@@ -67,11 +86,11 @@ class _SearchPageState extends State<SearchPage> {
         searchQuery = _searchQuery;
       });
       _fetchProductsData(searchQuery).then((_) {
-        if (products.length == 0) {
+        if (_products.length == 0) {
           showToast(msg: Translations.of(context).get('No results match your search'));
         } else {
-          var key = 'result${products.length > 1 ? "s" : ""} match your search';
-          showToast(msg: '${products.length} ${Translations.of(context).get(key)}');
+          var key = 'result${_products.length > 1 ? "s" : ""} match your search';
+          showToast(msg: '${_products.length} ${Translations.of(context).get(key)}');
         }
       }).catchError((error) {
         print('@error Error');
