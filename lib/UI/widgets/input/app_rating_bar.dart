922d8767a5d8daec6b6226461dfb24aa38bfb413
roaa94 <roaa94khaddam@gmail.com>
2021-03-28 14:41:36 +0300

    disable half rating

--- lib/UI/widgets/input/app_rating_bar.dart
+++ lib/UI/widgets/input/app_rating_bar.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:provider/provider.dart';
-import 'package:tiptop_v2/providers/app_provider.dart';
-import 'package:tiptop_v2/utils/styles/app_colors.dart';
 import 'package:tiptop_v2/utils/styles/app_text_styles.dart';
 
 class AppRatingBar extends StatelessWidget {
@@ -14,6 +10,7 @@ class AppRatingBar extends StatelessWidget {
   final bool disabled;
   final double starSize;
   final double starsGutter;
+  final bool hasHalfRating;
 
   AppRatingBar({
     this.initialRating = 0,
@@ -22,11 +19,11 @@ class AppRatingBar extends StatelessWidget {
     this.disabled = false,
     this.starSize = 20,
     this.starsGutter = 10,
+    this.hasHalfRating = false,
   });
 
   @override
   Widget build(BuildContext context) {
-    AppProvider appProvider = Provider.of<AppProvider>(context);
     return Row(
       children: <Widget>[
         RatingBar(
@@ -34,7 +31,7 @@ class AppRatingBar extends StatelessWidget {
           glow: false,
           initialRating: initialRating,
           direction: Axis.horizontal,
-          allowHalfRating: true,
+          allowHalfRating: hasHalfRating,
           itemCount: 5,
           itemSize: starSize,
           ratingWidget: RatingWidget(
