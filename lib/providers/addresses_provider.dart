64fd151c052c744a28aa509b293719bb1c18a0e1
roaa94 <roaa94khaddam@gmail.com>
2021-03-29 14:12:32 +0300

    minor fix

--- lib/providers/addresses_provider.dart
+++ lib/providers/addresses_provider.dart
@@ -76,11 +76,12 @@ class AddressesProvider with ChangeNotifier {
   Future<dynamic> fetchAndSetAddresses(AppProvider appProvider) async {
     final endpoint = 'profile/addresses';
     final responseData = await appProvider.get(endpoint: endpoint, withToken: true);
-    addressesResponseData = AddressesResponseData.fromJson(responseData);
-
     if (responseData == 401) {
       return 401;
     }
+
+    addressesResponseData = AddressesResponseData.fromJson(responseData);
+
 
     if (addressesResponseData.addressesData == null || addressesResponseData.status != 200) {
       throw HttpException(title: 'Error', message: addressesResponseData.message);
