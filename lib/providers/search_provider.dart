2ccde873fcd218f228c82f60616645653f8ed3b2
roaa94 <roaa94khaddam@gmail.com>
2021-04-04 01:31:59 +0300

    remove unused imports

--- lib/providers/search_provider.dart
+++ lib/providers/search_provider.dart
@@ -1,17 +1,9 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:tiptop_v2/UI/pages/location_permission_page.dart';
-import 'package:tiptop_v2/models/category.dart';
-import 'package:tiptop_v2/models/home.dart';
 import 'package:tiptop_v2/models/search.dart';
 import 'package:tiptop_v2/providers/app_provider.dart';
 import 'package:tiptop_v2/utils/http_exception.dart';
-import 'package:tiptop_v2/utils/location_helper.dart';
 
-import 'cart_provider.dart';
 import 'home_provider.dart';
-import 'local_storage.dart';
 
 class SearchProvider with ChangeNotifier {
   List<Term> terms = [];
