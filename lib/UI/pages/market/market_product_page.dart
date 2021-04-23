e59bb3ae45123475073d1a8720a4594fff98f46b
roaa94 <roaa94khaddam@gmail.com>
2021-04-23 13:36:39 +0300

    important fixes on market product page

--- lib/UI/pages/market/market_product_page.dart
+++ lib/UI/pages/market/market_product_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:tiptop_v2/UI/widgets/UI/app_loader.dart';
 import 'package:tiptop_v2/UI/widgets/UI/section_title.dart';
 import 'package:tiptop_v2/UI/widgets/formatted_prices.dart';
 import 'package:tiptop_v2/UI/widgets/market/cart_controls.dart';
@@ -57,6 +58,7 @@ class _MarketProductPageState extends State<MarketProductPage> {
   void didChangeDependencies() {
     if (_isInit) {
       Map<String, dynamic> data = ModalRoute.of(context).settings.arguments as Map<String, dynamic>;
+      product = data["product"];
       hasControls = data["has_controls"];
       productsProvider = Provider.of<ProductsProvider>(context);
       appProvider = Provider.of<AppProvider>(context);
@@ -94,86 +96,86 @@ class _MarketProductPageState extends State<MarketProductPage> {
   Widget build(BuildContext context) {
     return AppScaffold(
       bgColor: AppColors.white,
-      appBar: AppBar(
-        title: Text(product.title),
-        actions: [
-          if (appProvider.isAuth)
-            AnimatedOpacity(
-              duration: const Duration(milliseconds: 300),
-              opacity: _isLoadingProduct ? 0 : 1,
-              child: IconButton(
-                onPressed: _isLoadingInteractRequest ? null : interactWithProduct,
-                icon: AppIcons.icon(productIsFavorited ? FontAwesomeIcons.solidHeart : FontAwesomeIcons.heart),
-              ),
+      appBar: _isLoadingProduct
+          ? null
+          : AppBar(
+              title: Text(product.title),
+              actions: [
+                if (appProvider.isAuth)
+                  IconButton(
+                    onPressed: _isLoadingInteractRequest ? null : interactWithProduct,
+                    icon: AppIcons.icon(productIsFavorited ? FontAwesomeIcons.solidHeart : FontAwesomeIcons.heart),
+                  ),
+              ],
             ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: RefreshIndicator(
-              onRefresh: _fetchAndSetProduct,
-              child: ListView(
-                physics: AlwaysScrollableScrollPhysics(),
-                children: [
-                  AppCarousel(
-                    height: MediaQuery.of(context).size.width * 0.8,
-                    images: product.media.gallery.map((galleryItem) => galleryItem.file).toList(),
-                    hasIndicator: true,
-                    infinite: false,
+      body: _isLoadingProduct
+          ? AppLoader()
+          : Column(
+              children: [
+                Expanded(
+                  child: RefreshIndicator(
+                    onRefresh: _fetchAndSetProduct,
+                    child: ListView(
+                      physics: AlwaysScrollableScrollPhysics(),
+                      children: [
+                        AppCarousel(
+                          height: MediaQuery.of(context).size.width * 0.8,
+                          images: product.media.gallery.map((galleryItem) => galleryItem.file).toList(),
+                          hasIndicator: true,
+                          infinite: false,
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          product.title,
+                          style: AppTextStyles.h2,
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 20),
+                        FormattedPrices(
+                          price: product.price,
+                          discountedPrice: product.discountedPrice,
+                          isLarge: true,
+                        ),
+                        if (product.unitText != null) Text(product.unitText, style: AppTextStyles.subtitleXs50),
+                        const SizedBox(height: 20),
+                        if (product.description != null) SectionTitle('Details'),
+                        if (product.description != null)
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 9),
+                            child: Html(
+                              data: """${product.description.formatted}""",
+                            ),
+                          ),
+                      ],
+                    ),
                   ),
-                  const SizedBox(height: 20),
-                  Text(
-                    product.title,
-                    style: AppTextStyles.h2,
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 20),
-                  FormattedPrices(
-                    price: product.price,
-                    discountedPrice: product.discountedPrice,
-                    isLarge: true,
-                  ),
-                  if (product.unitText != null) Text(product.unitText, style: AppTextStyles.subtitleXs50),
-                  const SizedBox(height: 20),
-                  if (product.description != null) SectionTitle('Details'),
-                  if (product.description != null)
-                    Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 9),
-                      child: Html(
-                        data: """${product.description.formatted}""",
+                ),
+                if (hasControls)
+                  Container(
+                    padding: const EdgeInsets.only(
+                      top: listItemVerticalPadding,
+                      bottom: actionButtonBottomPadding,
+                      right: screenHorizontalPadding,
+                      left: screenHorizontalPadding,
+                    ),
+                    height: actionButtonContainerHeight,
+                    color: AppColors.bg,
+                    child: Container(
+                      constraints: BoxConstraints(maxHeight: buttonHeightSm),
+                      height: buttonHeightSm,
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(color: AppColors.shadow, blurRadius: 6),
+                        ],
+                      ),
+                      child: CartControls(
+                        isModalControls: true,
+                        product: product,
                       ),
                     ),
-                ],
-              ),
+                  )
+              ],
             ),
-          ),
-          if (hasControls)
-            Container(
-              padding: const EdgeInsets.only(
-                top: listItemVerticalPadding,
-                bottom: actionButtonBottomPadding,
-                right: screenHorizontalPadding,
-                left: screenHorizontalPadding,
-              ),
-              height: actionButtonContainerHeight,
-              color: AppColors.bg,
-              child: Container(
-                constraints: BoxConstraints(maxHeight: buttonHeightSm),
-                height: buttonHeightSm,
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(color: AppColors.shadow, blurRadius: 6),
-                  ],
-                ),
-                child: CartControls(
-                  isModalControls: true,
-                  product: product,
-                ),
-              ),
-            )
-        ],
-      ),
     );
   }
 }
