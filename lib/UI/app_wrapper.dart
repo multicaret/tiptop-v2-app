6911346ed7e6881b3dece920720245f9b99b173f
roaa94 <roaa94khaddam@gmail.com>
2021-04-23 22:07:09 +0300

    optimize app wrapper and enable pop to root when pressing on tab

--- lib/UI/app_wrapper.dart
+++ lib/UI/app_wrapper.dart
@@ -9,6 +9,7 @@ import 'package:tiptop_v2/UI/pages/home_page.dart';
 import 'package:tiptop_v2/UI/pages/market/market_search_page.dart';
 import 'package:tiptop_v2/UI/pages/profile/profile_page.dart';
 import 'package:tiptop_v2/UI/pages/support_page.dart';
+import 'package:tiptop_v2/UI/widgets/UI/app_scaffold.dart';
 import 'package:tiptop_v2/UI/widgets/cart/cart_fab.dart';
 import 'package:tiptop_v2/providers/app_provider.dart';
 import 'package:tiptop_v2/providers/home_provider.dart';
@@ -23,29 +24,31 @@ class AppWrapper extends StatefulWidget {
 
 class _AppWrapperState extends State<AppWrapper> {
   final CupertinoTabController _cupertinoTabController = CupertinoTabController();
+  int currentTabIndex = 0;
+  HomeProvider homeProvider;
 
-  List<GlobalKey<NavigatorState>> _getTabNavKeys(HomeProvider homeProvider) {
-    return List.generate(_getCupertinoTabsList(homeProvider).length, (i) => GlobalKey<NavigatorState>());
-  }
+  List<GlobalKey<NavigatorState>> _tabNavKeys = List.generate(_getCupertinoTabsList().length, (i) => GlobalKey<NavigatorState>());
 
-  GlobalKey<NavigatorState> currentNavigatorKey(HomeProvider homeProvider) {
-    List<GlobalKey<NavigatorState>> _tabNavKeys = _getTabNavKeys(homeProvider);
+  GlobalKey<NavigatorState> currentNavigatorKey() {
     return _tabNavKeys[_cupertinoTabController.index];
   }
 
-  void onTabItemTapped(int index) {
-    if (index == 2) {
-      return;
+  void onTabItemTapped(int index, HomeProvider homeProvider) {
+    if (_tabNavKeys[index].currentState != null && currentTabIndex == index) {
+      _tabNavKeys[index].currentState.popUntil((r) => r.isFirst);
     }
     _cupertinoTabController.index = index;
+    currentTabIndex = index;
   }
 
-  List<BottomNavigationBarItem> _getCupertinoTabBarItems(HomeProvider homeProvider, bool isRTL) {
-    List<Map<String, dynamic>> _cupertinoTabsList = _getCupertinoTabsList(homeProvider);
+  List<BottomNavigationBarItem> _getCupertinoTabBarItems(bool isRTL) {
+    List<Map<String, dynamic>> _cupertinoTabsList = _getCupertinoTabsList();
     double tabWidth = MediaQuery.of(context).size.width / _cupertinoTabsList.length;
+
     return List.generate(_cupertinoTabsList.length, (i) {
       int tabWithEndPaddingIndex = (_cupertinoTabsList.length / 2).ceil() - 1;
       int tabWithStartPaddingIndex = (_cupertinoTabsList.length / 2).ceil() + 1 - 1;
+
       return BottomNavigationBarItem(
         backgroundColor: AppColors.primary,
         icon: Padding(
@@ -66,7 +69,18 @@ class _AppWrapperState extends State<AppWrapper> {
     });
   }
 
-  List<Map<String, dynamic>> _getCupertinoTabsList(HomeProvider homeProvider) {
+  bool _isInit = true;
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      homeProvider = Provider.of<HomeProvider>(context);
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
+
+  static List<Map<String, dynamic>> _getCupertinoTabsList({bool channelIsMarket = true}) {
     return [
       {
         'title': 'Home',
@@ -75,7 +89,7 @@ class _AppWrapperState extends State<AppWrapper> {
       },
       {
         'title': 'Search',
-        'page': homeProvider.channelIsMarket ? MarketSearchPage() : FoodSearchPage(),
+        'page': channelIsMarket ? MarketSearchPage() : FoodSearchPage(),
         'icon': LineAwesomeIcons.search,
       },
       {
@@ -93,37 +107,41 @@ class _AppWrapperState extends State<AppWrapper> {
 
   @override
   Widget build(BuildContext context) {
-    // print('Rebuilt app wrapper');
-
-    return Consumer2<HomeProvider, AppProvider>(
-      child: CartFAB(),
-      builder: (c, homeProvider, appProvider, child) => WillPopScope(
-        onWillPop: () async {
-          return Platform.isAndroid ? !await currentNavigatorKey(homeProvider).currentState.maybePop() : null;
-        },
-        child: Stack(
-          children: [
-            CupertinoTabScaffold(
+    return WillPopScope(
+      onWillPop: () async {
+        return Platform.isAndroid ? !await currentNavigatorKey().currentState.maybePop() : null;
+      },
+      child: Stack(
+        children: [
+          Consumer<AppProvider>(
+            builder: (c, appProvider, _) => CupertinoTabScaffold(
               backgroundColor: AppColors.white,
               controller: _cupertinoTabController,
               tabBar: CupertinoTabBar(
-                onTap: onTabItemTapped,
+                onTap: (index) => onTabItemTapped(index, homeProvider),
+                currentIndex: currentTabIndex,
                 backgroundColor: AppColors.primary,
                 activeColor: AppColors.secondary,
                 inactiveColor: AppColors.white.withOpacity(0.5),
-                items: _getCupertinoTabBarItems(homeProvider, appProvider.isRTL),
+                items: _getCupertinoTabBarItems(appProvider.isRTL),
               ),
               tabBuilder: (BuildContext context, int index) {
                 return CupertinoTabView(
+                  navigatorKey: _tabNavKeys[index],
                   builder: (BuildContext context) {
-                    return _getCupertinoTabsList(homeProvider)[index]['page'];
+                    return _getCupertinoTabsList(channelIsMarket: homeProvider.channelIsMarket)[index]['page'];
+                  },
+                  onGenerateRoute: (settings) {
+                    return MaterialPageRoute(
+                      builder: (context) => AppScaffold(),
+                    );
                   },
                 );
               },
             ),
-            child
-          ],
-        ),
+          ),
+          CartFAB(),
+        ],
       ),
     );
   }
