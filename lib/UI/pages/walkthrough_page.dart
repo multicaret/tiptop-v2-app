5afb37e3d81581ddb53bf11de45f31e0e4f65c20
roaa94 <roaa94khaddam@gmail.com>
2021-04-27 19:29:32 +0300

    add lottie walkthrough animation

--- lib/UI/pages/walkthrough_page.dart
+++ lib/UI/pages/walkthrough_page.dart
@@ -1,74 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
+import 'package:tiptop_v2/UI/app_wrapper.dart';
 import 'package:tiptop_v2/UI/pages/location_permission_page.dart';
 import 'package:tiptop_v2/UI/pages/otp/otp_choose_method_page.dart';
 import 'package:tiptop_v2/UI/widgets/UI/app_scaffold.dart';
 import 'package:tiptop_v2/i18n/translations.dart';
+import 'package:tiptop_v2/providers/app_provider.dart';
 import 'package:tiptop_v2/utils/constants.dart';
 import 'package:tiptop_v2/utils/location_helper.dart';
 import 'package:tiptop_v2/utils/styles/app_buttons.dart';
 import 'package:tiptop_v2/utils/styles/app_colors.dart';
 import 'package:tiptop_v2/utils/styles/app_text_styles.dart';
 
-import 'package:tiptop_v2/UI/app_wrapper.dart';
-
 class WalkthroughPage extends StatelessWidget {
   static const routeName = '/walkthrough';
 
   @override
   Widget build(BuildContext context) {
-    Size screenSize = MediaQuery.of(context).size;
-
     return AppScaffold(
       bodyPadding: const EdgeInsets.symmetric(horizontal: screenHorizontalPadding),
       bgColor: AppColors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Image.asset(
-            'assets/images/tiptop-logo.png',
-            width: screenSize.width / 2.5,
-          ),
-          Container(
-            child: Column(
-              children: [
-                TextButton(
-                  onPressed: () {
-                    getLocationPermissionStatus().then((isGranted) {
-                      Navigator.of(context).pushReplacementNamed(isGranted ? AppWrapper.routeName : LocationPermissionPage.routeName);
-                    });
-                  },
-                  child: Text(
-                    Translations.of(context).get('Continue Without Login'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Consumer<AppProvider>(
+              builder: (c, appProvider, _) {
+                print(appProvider.appLocale.languageCode);
+                return Container(
+                  width: double.infinity,
+                  child: Lottie.asset(
+                    'assets/images/lottie-walkthrough/walkthrough-${appProvider.appLocale.languageCode}.json',
+                    repeat: false,
+                    fit: BoxFit.cover,
+                    width: double.infinity,
                   ),
-                ),
-                const SizedBox(height: 40),
-                AppButtons.primary(
-                  onPressed: () {
-                    Navigator.of(context).pushReplacementNamed(OTPChooseMethodPage.routeName);
-                  },
-                  child: Text(Translations.of(context).get('Register')),
-                ),
-                const SizedBox(height: 40),
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pushNamed(OTPChooseMethodPage.routeName);
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(Translations.of(context).get('Already have an account?')),
-                      const SizedBox(width: 5),
-                      Text(
-                        Translations.of(context).get('Login'),
-                        style: AppTextStyles.bodySecondaryDark,
-                      ),
-                    ],
-                  ),
-                )
-              ],
+                );
+              },
             ),
-          ),
-        ],
+            const SizedBox(height: 20),
+            Container(
+              child: Column(
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      getLocationPermissionStatus().then((isGranted) {
+                        Navigator.of(context).pushReplacementNamed(isGranted ? AppWrapper.routeName : LocationPermissionPage.routeName);
+                      });
+                    },
+                    child: Text(
+                      Translations.of(context).get('Continue Without Login'),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  AppButtons.primary(
+                    onPressed: () {
+                      Navigator.of(context).pushReplacementNamed(OTPChooseMethodPage.routeName);
+                    },
+                    child: Text(Translations.of(context).get('Register')),
+                  ),
+                  const SizedBox(height: 20),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(OTPChooseMethodPage.routeName);
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(Translations.of(context).get('Already have an account?')),
+                        const SizedBox(width: 5),
+                        Text(
+                          Translations.of(context).get('Login'),
+                          style: AppTextStyles.bodySecondaryDark,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
