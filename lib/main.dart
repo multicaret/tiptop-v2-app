0dd1f1ffa133fa9045f925126df8f9243d6e238d
KawsaraMohamed MK <mkwsra@gmail.com>
2021-05-08 03:19:24 +0300

    remove old Adfuck sorry, Adjust* init. and init UniLinks to handle a steam of incomming URLs from native

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,10 @@ import 'package:tiptop_v2/providers.dart';
 import 'package:tiptop_v2/providers/app_provider.dart';
 import 'package:tiptop_v2/providers/local_storage.dart';
 import 'package:tiptop_v2/routes.dart';
-import 'package:tiptop_v2/utils/adjust.dart';
 import 'package:tiptop_v2/utils/styles/app_colors.dart';
 import 'package:tiptop_v2/utils/styles/app_text_styles.dart';
 import 'package:tiptop_v2/utils/event_tracking.dart';
+import 'package:uni_links/uni_links.dart';
 
 import 'UI/app_wrapper.dart';
 import 'UI/pages/force_update_page.dart';
@@ -33,7 +33,6 @@ void main() async {
       // LocalStorage().deleteData(key: 'selected_address');
       AppProvider appProvider = AppProvider();
       appProvider.initInstaBug();
-      appProvider.initAdjust();
       await appProvider.bootActions();
       runApp(MyApp(
         appProvider: appProvider,
@@ -54,6 +53,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
   Future<void> _autoLoginFuture;
   EventTracking eventTracking = EventTracking.getActions();
+  StreamSubscription _deepLinksSubscription;
 
   Future<void> _initEventTracking() async {
     await eventTracking.initEventTracking();
@@ -64,14 +64,94 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
       'user_language': widget.appProvider.appLocale.languageCode,
     };
     await eventTracking.trackEvent(TrackingEvent.VISIT, visitEventParams);
+
+    // Attach a listener to the stream
+    _deepLinksSubscription = uriLinkStream.listen((Uri uri) {
+      print("Got a deeeeep deep link: 💩💩💩💩💩💩💩");
+      if (uri != null) {
+        String mainAction = uri.host;
+        var params = uri.queryParametersAll;
+        String uriChannel = params['channel'] as String;
+        String itemId = params['id'] as String;
+        String itemParentId = params['parent_id'] as String;
+        // Some Flags
+        bool hasId = itemId != null;
+        bool hasParentId = itemParentId != null;
+        bool hasChannel = uriChannel != null;
+        if (mainAction != null) {
+          switch (mainAction) {
+            case "blog_index":
+              // Todo: Open Blog page:
+              print("Open Blog page:");
+              break;
+            case "blog_show":
+              if (hasId) {
+                // Todo: Open Blog show page: using {itemId}
+                print("Open Blog show page: using {itemId}");
+              }
+              break;
+            case "favorites":
+              if (hasChannel) {
+                // Todo: Open Favorites page: using {uriChannel}
+                print("Open Favorites page: using {uriChannel}");
+              }
+              break;
+            case "addresses":
+              // Todo: Open Addresses page
+              print("Open Addresses page");
+              break;
+            case "home_screen_by_channel":
+              if (hasChannel) {
+                // Todo: Open Home page: using {uriChannel}
+                print("Open Home page: using {uriChannel}");
+              }
+              break;
+            case "market_food_category_show":
+              if (hasChannel && hasId && hasParentId) {
+                // Todo: Open Food Branch scroll to category: using: { uriChannel, itemId, itemParentId}
+                print("Open Food Branch scroll to category: using: { uriChannel, itemId, itemParentId}");
+                // OR
+                // Todo: Open Category Market scroll to category: using: { uriChannel, itemId, itemParentId}
+                print("Open Category Market scroll to category: using: { uriChannel, itemId, itemParentId}");
+              }
+              break;
+            case "order_rating":
+              if (hasId) {
+                // Todo: Open Order Rating screen: using: {itemId}
+                print("Open Order Rating screen: using: {itemId}");
+              }
+              break;
+            case "order_tracking":
+              if (hasId) {
+                // Todo: Open Order Tracking screen: using: {itemId}
+                print("Open Order Tracking screen: using: {itemId}");
+              }
+              break;
+            case "previous_orders":
+              if (hasChannel) {
+                // Todo: Open Previous Orders page: using {uriChannel}
+                print("Open Previous Orders page: using {uriChannel}");
+              }
+              break;
+            case "product_show":
+              if (hasId && hasChannel) {
+                // Todo: Open Product Show screen: using {itemId,hasChannel}
+                print("Open Product Show screen: using {itemId,hasChannel}");
+              }
+              break;
+          }
+        }
+      }
+      // Use the uri and warn the user, if it is not correct
+    }, onError: (err) {
+      // Handle exception by warning the user their action did not succeed
+    });
   }
 
   @override
   void initState() {
     _autoLoginFuture = widget.appProvider.autoLogin();
     WidgetsBinding.instance.addObserver(this);
-    getDeepLinkUrlFromPlugin();
-
     super.initState();
 
     //Init tracking services
@@ -81,6 +161,7 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
   @override
   void dispose() {
     WidgetsBinding.instance.removeObserver(this);
+    _deepLinksSubscription.cancel();
     super.dispose();
   }
 
@@ -90,32 +171,12 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
       case AppLifecycleState.inactive:
         break;
       case AppLifecycleState.resumed:
-        //Todo: track visit event again???
         Adjust.onResume();
         break;
       case AppLifecycleState.paused:
         Adjust.onPause();
         break;
-      case AppLifecycleState.detached:
-        break;
     }
-    // Todo: To get fresh data on all states
-    print('state = $state');
-    getDeepLinkUrlFromPlugin();
-  }
-
-  // Todo: Move this class to somewhere else
-  Future<void> getDeepLinkUrlFromPlugin() async {
-    String deepLinkUrl;
-    try {
-      deepLinkUrl = await DeepLinkPlugin.getUrl;
-    } on PlatformException {
-      deepLinkUrl = 'Error on init deep link plugin';
-    }
-    if (!mounted) return;
-
-    print("got a deeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeep link:");
-    print(deepLinkUrl);
   }
 
   @override
